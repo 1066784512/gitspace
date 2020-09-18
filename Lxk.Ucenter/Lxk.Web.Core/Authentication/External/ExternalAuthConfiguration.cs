e299f693f595f2a706bc7a9a573605bd0a539ed0
晓凯 李 <li1066784512@hotmail.com>
2020-09-18 18:00:30 +0800

    新增配置

---
+++ Lxk.Ucenter/Lxk.Web.Core/Authentication/External/ExternalAuthConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Abp.Dependency;
+using System.Collections.Generic;
+
+namespace Lxk.Ucenter.Web.Core.Authentication.External
+{
+    public class ExternalAuthConfiguration : IExternalAuthConfiguration, ISingletonDependency
+    {
+        public List<ExternalLoginProviderInfo> Providers { get; }
+
+        public ExternalAuthConfiguration()
+        {
+            Providers = new List<ExternalLoginProviderInfo>();
+        }
+    }
+}
